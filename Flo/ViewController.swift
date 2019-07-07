24e2233ee2afe9a303e8ab22385b84776b94dbea
nkazakova <natalia.kazakova@innoway.ru>
2019-07-07 15:32:16 +0300

    Initial commit

---
+++ Flo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flo
+//
+//  Created by Natalia Kazakova on 07/07/2019.
+//  Copyright © 2019 Natalia Kazakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
